3235ec3d51fc19484efbbfd58466f327817bf7e1
bendy1234 <83143663+bendy1234@users.noreply.github.com>
2025-03-02 11:00:22 -0800

    Add modmenu fabric api dependencies

--- src/main/java/dev/notalpha/dashloader/client/ModMenuCompat.java
+++ src/main/java/dev/notalpha/dashloader/client/ModMenuCompat.java
@@ -5,7 +5,7 @@ import com.terraformersmc.modmenu.api.ModMenuApi;
 import dev.notalpha.dashloader.client.ui.ConfigScreen;
 import net.minecraft.client.gui.screen.Screen;
 
-public class modMenuCompat implements ModMenuApi {
+public class ModMenuCompat implements ModMenuApi {
 	@Override
 	public ConfigScreenFactory<Screen> getModConfigScreenFactory() {
 		return ConfigScreen::new;
